fe380b67766589a0019e9993812d4a87e163c9e0
HiradJF <hiradjahangirfard@gmail.com>
2026-07-02 15:36:48 +0330

    Added random_u32 function.

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t random_u32(){
+
+#if defined(__linux__) 
+#include <sys/random.h>
+
+uint32_t rand_int;
+if (getrandom(rand_int, sizeof(rand_int), 0) != sizeof(rand_int)){
+    perror("randbit");
+    exit(1);
+}
+return rand_int;
+
+#elif defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#include <stdlib.h>
+
+uint32_t rand_int;
+arc4random_buf(&rand_int, sizeof(rand_int));
+return rand_int;
+
+#elif defined(_WIN32)
+#include <windows.h>
+#include <bcrypt.h>
+
+uint32_t rand_int;
+BCryptGenRandom(NULL, (PUCHAR)&rand_int, sizeof(rand_int), BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+
+#endif
+
+}
