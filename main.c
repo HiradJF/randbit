bbe339af01fc6fc62bdba0d8485fb96abd5156a4
HiradJF <hiradjahangirfard@gmail.com>
2026-07-12 22:28:53 +0330

    Fixed function indentation bug

--- main.c
+++ main.c
@@ -17,36 +17,41 @@ static inline bool streq(const char *a, const char *b){
     return strcmp(a, b) == 0;
 }
 
-static inline uint32_t random_u32(){
 
-#if defined(__linux__) 
+
+#ifdef __linux__
 #include <sys/random.h>
 
-uint32_t rand_int;
+static inline uint32_t random_u32(){
+    uint32_t rand_int;
 
-if (getrandom((void*)&rand_int, sizeof(rand_int), 0) != sizeof(rand_int)){
-    perror("randbit: getrandom");
-    fputs("randbit: random number generation failed.", stderr);
-    exit(EXIT_GEN_ERR);
+    if (getrandom((void*)&rand_int, sizeof(rand_int), 0) != sizeof(rand_int)){
+        perror("randbit: getrandom");
+        fputs("randbit: random number generation failed.", stderr);
+        exit(EXIT_GEN_ERR);
+    }
+    return rand_int;
 }
-return rand_int;
 
 #elif defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
 #include <stdlib.h>
 
-uint32_t rand_int;
-arc4random_buf(&rand_int, sizeof(rand_int));
-return rand_int;
+static inline uint32_t random_u32(){
+    uint32_t rand_int;
+    arc4random_buf(&rand_int, sizeof(rand_int));
+    return rand_int;
+}
 
 #elif defined(_WIN32)
 #include <windows.h>
 #include <bcrypt.h>
-
-uint32_t rand_int;
-BCryptGenRandom(NULL, (PUCHAR)&rand_int, sizeof(rand_int), BCRYPT_USE_SYSTEM_PREFERRED_RNG);
-
-#endif
+static inline uint32_t random_u32(){
+    uint32_t rand_int;
+    BCryptGenRandom(NULL, (PUCHAR)&rand_int, sizeof(rand_int), BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+    return rand_int; 
 }
+#endif
+
 
 static inline bool rand_bit(){
     return random_u32() & 1;
